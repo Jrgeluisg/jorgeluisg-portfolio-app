0551963c0a2e04db02a66163f9b8b5bf90eeed5f
Jorge Luis Galarraga <jorgeluis.galarraga@cl.abstracta.us>
2020-12-01 23:29:32 -0300

    Add the ability to receive timestamp if a string query is given or if it is left blank

--- server.js
+++ server.js
@@ -27,10 +27,36 @@ app.get("/api/hello", function (req, res) {
 });
 
 
+/**
+A request to /api/timestamp/:date? with a valid date should return a JSON object with a unix key that is a Unix timestamp of the input date in milliseconds
+A request to /api/timestamp/:date? with a valid date should return a JSON object with a utc key that is a string of the input date in the format: Thu, 01 Jan 1970 00:00:00 GMT
+A request to /api/timestamp/1451001600000 should return { unix: 1451001600000, utc: "Fri, 25 Dec 2015 00:00:00 GMT" } 
+Your project can handle dates that can be successfully parsed by new Date(date_string)
+An empty date parameter should return the current time in a JSON object with a unix key
+An empty date parameter should return the current time in a JSON object with a utc key */
+
+app.get('/api/timestamp', (req, res) => {
+  let now = new Date();
+  res.json({
+    "unix": now.getTime(),
+    "utc": now.toUTCString()
+  })
+})
+
 app.get('/api/timestamp/:date_string?', (req, res) => {
   let dateString = req.params.date_string;
-  console.log(dateString);
-  res.json({"error":"Invalid Date"});
+  let passedInValue = new Date(dateString);
+
+  if (passedInValue == "Invalid Date") {
+    res.json({"error":"Invalid Date"});
+  } else {
+    res.json(
+      {
+        "unix": passedInValue.getTime(),
+        "utc": passedInValue.toUTCString()
+      }
+    );
+  }  
 });
 
 
